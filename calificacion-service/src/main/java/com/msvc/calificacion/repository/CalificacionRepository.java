12a36552c3a6bc32b3aa6b14793821cea210737d
jchavarro <josechavarrorojas@gmail.com>
2024-05-09 18:01:40 -0500

    calificacion-service

---
+++ calificacion-service/src/main/java/com/msvc/calificacion/repository/CalificacionRepository.java
@@ -0,0 +1,12 @@
+package com.msvc.calificacion.repository;
+
+import com.msvc.calificacion.entity.Calificacion;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface CalificacionRepository extends MongoRepository<Calificacion,Long> {
+
+    List<Calificacion> findByUsuarioId(String usuarioId);
+    List<Calificacion> findByHotelId(String hotelId);
+}
